6b9307afef3ccf6d9777ffa640a7bb1e7923d083
Ho Ngu Dat <hongudat@gmail.com>
2023-10-24 17:33:18 +0700

    Init project

---
+++ ASP.NETCORE_PROJECT/ASP.NETCORE_PROJECT/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP.NETCORE_PROJECT.Models;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NETCORE_PROJECT.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+    [Required(ErrorMessage = "Please input full name !")]
+    public string FullName { get; set; }
+    [Required(ErrorMessage = "Please input address!")]
+    public string Address { get; set; }
+    public virtual ICollection<Comment> Comments { get; set; }
+    public virtual ICollection<Order> Orders { get; set; }
+}
+
---
+++ ASP.NETCORE_PROJECT/ASP.NETCORE_PROJECT/Migrations/20230605182306_updateorder.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASP.NETCORE_PROJECT.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateorder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "order_status",
+                table: "Order",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "order_status",
+                table: "Order");
+        }
+    }
+}
